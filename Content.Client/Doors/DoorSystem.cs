99a23ab9baaec5f08f25684d859695cdb8675c90
Perry Fraser <perryprog@users.noreply.github.com>
2026-04-16 18:24:20 -0400

    fix: allow opening doors with open wire panels (#38733)

--- Content.Client/Doors/DoorSystem.cs
+++ Content.Client/Doors/DoorSystem.cs
@@ -47,8 +47,8 @@ public sealed class DoorSystem : SharedDoorSystem
     protected override void OnComponentInit(Entity<DoorComponent> ent, ref ComponentInit args)
     {
         var comp = ent.Comp;
-        comp.OpenSpriteStates = new List<(DoorVisualLayers, string)>(2);
-        comp.ClosedSpriteStates = new List<(DoorVisualLayers, string)>(2);
+        comp.OpenSpriteStates = new List<(Enum, string)>(2);
+        comp.ClosedSpriteStates = new List<(Enum, string)>(2);
 
         comp.OpenSpriteStates.Add((DoorVisualLayers.Base, comp.OpenSpriteState));
         comp.ClosedSpriteStates.Add((DoorVisualLayers.Base, comp.ClosedSpriteState));
@@ -163,6 +163,9 @@ public sealed class DoorSystem : SharedDoorSystem
 
                 foreach (var (layer, layerState) in entity.Comp.OpenSpriteStates)
                 {
+                    // Allow animations to play while it's open (e.g., pinion);
+                    // the animation unsets this so we gotta set it again.
+                    _sprite.LayerSetAutoAnimated((entity.Owner, sprite), layer, true);
                     _sprite.LayerSetRsiState((entity.Owner, sprite), layer, layerState);
                 }
 
@@ -179,6 +182,7 @@ public sealed class DoorSystem : SharedDoorSystem
 
                 foreach (var (layer, layerState) in entity.Comp.ClosedSpriteStates)
                 {
+                    _sprite.LayerSetAutoAnimated((entity.Owner, sprite), layer, true);
                     _sprite.LayerSetRsiState((entity.Owner, sprite), layer, layerState);
                 }
 
--- Content.Shared/Doors/Components/DoorComponent.cs
+++ Content.Shared/Doors/Components/DoorComponent.cs
@@ -188,7 +188,7 @@ public sealed partial class DoorComponent : Component
     /// The sprite states used for the door while it's open.
     /// </summary>
     [ViewVariables(VVAccess.ReadOnly)]
-    public List<(DoorVisualLayers, string)> OpenSpriteStates = default!;
+    public List<(Enum, string)> OpenSpriteStates = default!;
 
     /// <summary>
     /// The sprite state used for the door when it's closed.
@@ -201,7 +201,7 @@ public sealed partial class DoorComponent : Component
     /// The sprite states used for the door while it's closed.
     /// </summary>
     [ViewVariables(VVAccess.ReadOnly)]
-    public List<(DoorVisualLayers, string)> ClosedSpriteStates = default!;
+    public List<(Enum, string)> ClosedSpriteStates = default!;
 
     /// <summary>
     /// The sprite state used for the door when it's opening.
