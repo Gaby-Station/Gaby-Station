d5dc63a93852560c4760c7f2fdd847f5189040cb
Baptr0b0t <152836416+Baptr0b0t@users.noreply.github.com>
2026-06-08 13:58:01 +0200

    Fix crit/dead/unconscious/stunned escape pulling (#6645)
    FixCritpulling
    
    Co-authored-by: Baptr0b0t <Baptr0b0t@fake.email>

--- Content.Shared/Movement/Pulling/Systems/PullingSystem.cs
+++ Content.Shared/Movement/Pulling/Systems/PullingSystem.cs
@@ -360,6 +360,9 @@ public sealed class PullingSystem : EntitySystem
         if (args.Handled)
             return;
 
+        if (!_blocker.CanInteract(ent, null))
+            return;
+
         args.Handled = TryStopPull(ent, ent, ent);
     }
 
