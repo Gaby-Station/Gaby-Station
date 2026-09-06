80db8d55900f19f587100c6e321dbc79e62b0255
Lucasalemao10 <lucasaugustooliveira3000@gmail.com>
2026-09-06 05:37:04 -0300

    adiciona tradução, arruma tradução e ajusta o tamanho dos botões no forms

--- Content.Client/_Starlight/UserInterface/RichText/SignatureTagHandler.cs
+++ Content.Client/_Starlight/UserInterface/RichText/SignatureTagHandler.cs
@@ -85,9 +85,9 @@ public sealed class SignatureTagHandler : IMarkupTagHandler
     {
         var btn = new Button
         {
-            Text = "Sign",
-            MinSize = new Vector2(48, FontLineHeight + 4),
-            MaxSize = new Vector2(48, FontLineHeight + 4),
+            Text = Loc.GetString("paper-signature-sign-button"),
+            MinSize = new Vector2(68, FontLineHeight + 4),
+            MaxSize = new Vector2(68, FontLineHeight + 4),
             Margin = new Thickness(1, 2, 1, 2),
             StyleClasses = { "ButtonSquare" },
             TextAlign = Label.AlignMode.Center
